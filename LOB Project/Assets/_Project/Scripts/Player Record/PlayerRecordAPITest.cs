8d44157527fa8ad0d35304a59d9cbb70fc3409b4
DaeStan <stanifed@my.erau.edu>
2026-04-07 10:44:03 -0700

    Connect Unity to Laravel Backend for Player Record

---
+++ LOB Project/Assets/_Project/Scripts/Player Record/PlayerRecordAPITest.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+
+/* This is a template file for API Testing.
+ * It is incomplete and needs to be filled out with information for specific endpoints.
+ * COPY AND PASTE THIS FILE, RENAME IT TO {FEATURE}APITest
+ * Fill out any commented lines with the relevent information
+ * 
+ * Go to the API Test Scene. Add this script to an object 
+ * and hook it up to the button and input field.
+ * Test it and pray for success.
+ */
+
+public class PlayerRecordAPITest : MonoBehaviour
+{
+    // Replace type with your backend manager
+    [SerializeField] private BackendPlayerRecordConnector apiManager;
+
+    [Header("UI Components")]
+    [SerializeField] private TextMeshProUGUI urlDisplay;
+    [SerializeField] private TMP_InputField urlField;
+
+    private void Awake()
+    {
+        // uncomment
+        urlField.text = apiManager.GetURL();
+        DisplayURL();
+        DebugObjects();
+    }
+
+    public void ChangeURL(TMP_InputField field)
+    {
+         apiManager.SetURL(field.text);
+         DisplayURL();
+    }
+
+    public void DebugIndexMethod()
+    {
+        DebugObjects();
+    }
+
+    private void DisplayURL()
+    {
+         urlDisplay.text = "Current URL: " + apiManager.GetURL();
+    }
+
+    private async void DebugObjects()
+    {
+        // replace string with your container and make sure the index method is correct
+        List<PlayerRecord> items = await apiManager.GetIndexAsync();
+
+
+        if (items != null)
+        {
+            Debug.Log("API Call was a success");
+            // replace string with your object type
+            foreach (PlayerRecord item in items)
+            {
+                // Access the shape name from the full object data
+                Debug.Log("Object in DB: " + item);
+            }
+        }
+        else if (items == null)
+        {
+            Debug.Log("Your request probably failed.");
+        }
+    }
+}
