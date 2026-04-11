9cbbb97d700812c32287adcc7cb8c4db361b2698
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2026-04-11 16:05:41 -0700

    Create Clickable Script

---
+++ LOB Project/Assets/_Project/Scripts/Chat Log/Clickable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject clickedObject = hit.collider.gameObject;
+                Debug.Log("You clicked on " + clickedObject.name);
+            }
+        }
+    }
+}
