020209efc20db3fd36f4c3d67056c1137f61fbfb
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2026-04-11 18:07:23 -0700

    Add Computer Camera and Functionality

--- LOB Project/Assets/_Project/Scripts/Chat Log/Clicker.cs
+++ LOB Project/Assets/_Project/Scripts/Chat Log/Clicker.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Clicker : MonoBehaviour
 {
+    [SerializeField] private Clicker player;
+    [SerializeField] private float max_distance;
+
+    private GameObject clickedObject;
+    private bool isSwitched;
+    private float distance;
+
+    public SwitchCams switcher;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Left mouse button
@@ -13,9 +22,37 @@ public class Clicker : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                GameObject clickedObject = hit.collider.gameObject;
-                Debug.Log("You clicked on " + clickedObject.name);
+                clickedObject = hit.collider.gameObject;
+                //Debug.Log("You clicked on " + clickedObject.name);
             }
+            else
+            { 
+                clickedObject = null;
+            }
+
+
+            if (clickedObject != null)
+            {
+                distance = Vector3.Distance(player.transform.position, clickedObject.transform.position);
+            }
+
+            //Debug.Log("Distance from " + clickedObject + ": " + distance);
+
+            if (distance <= max_distance)
+            {
+                Debug.Log("You Selected " + clickedObject + " at distance " + distance);
+                switcher.SwitchCameras();
+                isSwitched = true;
+            }
+                
         }
+
+        if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Tab)) && isSwitched)
+        {
+            switcher.SwitchCameras();
+            isSwitched = false;
+            
+        }
+
     }
 }
