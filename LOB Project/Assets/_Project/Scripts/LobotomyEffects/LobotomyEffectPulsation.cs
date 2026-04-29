2b11f176e10f838784240919fb40e375c64d7d49
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2026-04-29 13:20:38 -0700

    Add Pulsation Effect

--- LOB Project/Assets/_Project/Scripts/LobotomyEffects/LobotomyEffectPulsation.cs
+++ LOB Project/Assets/_Project/Scripts/LobotomyEffects/LobotomyEffectPulsation.cs
@@ -1,18 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LobotomyEffectPulsation : MonoBehaviour
+public class LobotomyEffectPulsation : MonoBehaviour, ILobotomyEffect
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject heartSoundPrefab;
+    [Range(0.1f, 2)][SerializeField] private float pulseRate;
+    [Range(0.1f, 100)][SerializeField] private float _selectionWeight;
+
+    public float GetEffectSelectionPriority()
     {
-        
+        return _selectionWeight;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartEffect(Transform selection)
     {
-        
+        GameObject heartsfxObject = Instantiate(heartSoundPrefab, selection);
+
+        AudioSource soundSource = heartsfxObject.GetComponent<AudioSource>();
+        Destroy(heartsfxObject, soundSource.clip.length);
+
+        StartCoroutine(DoPulsation(selection, soundSource.clip.length));
     }
+
+    private IEnumerator DoPulsation(Transform selection, float length)
+    {
+        float timer = length;
+        bool pulseOut = true;
+
+        Vector3 startScale = selection.localScale;
+
+        while (timer > 0)
+        {
+            if (pulseOut)
+            {
+                if (selection.localScale.magnitude > startScale.magnitude * 1.5f)
+                    pulseOut = !pulseOut;
+
+                selection.localScale += Vector3.one * Time.deltaTime * pulseRate;
+            }
+            else
+            {
+                if (selection.localScale.magnitude < startScale.magnitude)
+                    pulseOut = !pulseOut;
+
+                selection.localScale -= Vector3.one * Time.deltaTime;
+            }
+
+            yield return null;
+        }
+    }
+
+    public void StopEffect(Transform selection) { }
 }
